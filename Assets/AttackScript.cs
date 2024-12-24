cf332e62f4d8534109474a9bf66d185fcc5efca5
AlekOneTap <sashashapovalov04@gmail.com>
2024-12-24 12:38:04 +0300

    Анимка атаки
    При нажатии на лкм идет анимация атаки ( пока что цилиндром)

---
+++ Assets/AttackScript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class AttackScript : MonoBehaviour
+{
+    Animator animator;
+    void Start()
+    {
+      animator = GetComponent<Animator>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)) animator.SetBool("Attack", true);
+        else if (Input.GetMouseButtonUp(0)) animator.SetBool("Attack",false);
+    }
+}
