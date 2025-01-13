0805eb13fb2eda14e165cc944480c4c7652a32a3
hikk1ch <hikk1ch@mail.ru>
2025-01-14 00:26:19 +1000

    Fix sounds
    Add:
    sound attak
    fix:
    footsteps

--- Assets/Scripts/Mechanics/AttackScript.cs
+++ Assets/Scripts/Mechanics/AttackScript.cs
@@ -3,15 +3,28 @@ using UnityEngine;
 public class AttackScript : MonoBehaviour
 {
     Animator animator;
+    public AudioSource swordSound;  // Ссылка на компонент AudioSource
+    public AudioClip attackSound;   // Звук удара
+
     void Start()
     {
-      animator = GetComponent<Animator>();   
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0)) animator.SetBool("IsAttacking", true);
-        else if (Input.GetMouseButtonUp(0)) animator.SetBool("IsAttacking", false);
+        if (Input.GetMouseButtonDown(0))
+        {
+            animator.SetBool("IsAttacking", true);
+            if (swordSound != null && attackSound != null)
+            {
+                swordSound.PlayOneShot(attackSound);  // Воспроизведение звука удара
+            }
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            animator.SetBool("IsAttacking", false);
+        }
     }
 }
--- Assets/Scripts/Mechanics/MovementController.cs
+++ Assets/Scripts/Mechanics/MovementController.cs
@@ -5,31 +5,27 @@ using UnityEngine.InputSystem;
 public class MovementController : MonoBehaviour
 {
     public Transform Camera;
-
     public CharacterController CharacterController;
-
     public PlayerInput Input;
-
     public float movespeed;
     private InputAction _move;
-
     private InputAction _look;
     public float Sensivity;
-
     private InputAction _jump;
-
     public float jumpforce;
     public LayerMask groundLayer;
     public Transform groundCheck;
     public float groundCheckRadius = 0.01f;
-
 
     private bool _isGrounded;
     private float _yRotation;
     private Vector3 _velocity;
     public float Gravity;
 
+    // Для звука шагов
+    public AudioSource FootstepAudioSource;
 
+    private bool _isMoving;
 
     void Start()
     {
@@ -41,16 +37,16 @@ public class MovementController : MonoBehaviour
 
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = true;
-
-
-
-
-
-
     }
 
-
     void Update()
+    {
+        HandleLook();
+        HandleMovement();
+        HandleFootstepSound();
+    }
+
+    private void HandleLook()
     {
         var LookInputX = _look.ReadValue<Vector2>().x * Sensivity * Time.deltaTime;
         var LookInputY = -_look.ReadValue<Vector2>().y * Sensivity * Time.deltaTime;
@@ -60,7 +56,10 @@ public class MovementController : MonoBehaviour
         Camera.localRotation = Quaternion.Euler(_yRotation, 0f, 0f);
 
         transform.Rotate(0, LookInputX, 0);
+    }
 
+    private void HandleMovement()
+    {
         _velocity.y += Gravity * Time.deltaTime;
 
         var MoveInput = _move.ReadValue<Vector2>() * movespeed * Time.deltaTime;
@@ -69,29 +68,39 @@ public class MovementController : MonoBehaviour
 
         if (CharacterController.isGrounded)
         {
-
             if (_jump.triggered)
             {
-
                 _velocity.y = jumpforce;
             }
         }
+
         CharacterController.Move(MoveDirection);
         CharacterController.Move(_velocity);
 
-
-
-
-
-
-
-
-
-
+        // Определяем, движется ли персонаж
+        _isMoving = CharacterController.isGrounded && MoveInput.magnitude > 0;
     }
+
+    private void HandleFootstepSound()
+    {
+        if (_isMoving)
+        {
+            if (!FootstepAudioSource.isPlaying)
+            {
+                FootstepAudioSource.Play(); // Начать воспроизведение, если звук не играет
+            }
+        }
+        else
+        {
+            if (FootstepAudioSource.isPlaying)
+            {
+                FootstepAudioSource.Stop(); // Остановить воспроизведение, если персонаж остановился
+            }
+        }
+    }
+
     private void _jump_performed(InputAction.CallbackContext obj)
     {
-        throw new System.NotImplementedException();
+        // Логика прыжка, если потребуется
     }
-
 }
